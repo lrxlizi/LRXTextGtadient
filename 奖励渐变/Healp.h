c51aeee8b33599f1d4f7624562097e312c7b9b27
liruixia <liruixia@csdamedia.com>
2017-12-14 11:16:16 +0800

    control 渐变添加

--- 奖励渐变/Healp.h
+++ 奖励渐变/Healp.h
@@ -10,5 +10,16 @@
 #import <UIKit/UIKit.h>
 
 @interface Healp : NSObject
-+(void)TextGradientview:(UIView *)view bgVIew:(UIView *)bgVIew gradientColors:(NSArray *)colors gradientStartPoint:(CGPoint)startPoint endPoint:(CGPoint)endPoint ;
+/*
+ view 是要设置渐变字体的控件   bgVIew是view的父视图  colors是渐变的组成颜色  startPoint是渐变开始点 endPoint结束点
+ */
++(void)TextGradientview:(UIView *)view bgVIew:(UIView *)bgVIew gradientColors:(NSArray *)colors gradientStartPoint:(CGPoint)startPoint endPoint:(CGPoint)endPoint;
+/*
+ control 是要设置渐变字体的控件   bgVIew是view的父视图  colors是渐变的组成颜色  startPoint是渐变开始点 endPoint结束点
+ */
++(void)TextGradientControl:(UIControl *)control bgVIew:(UIView *)bgVIew gradientColors:(NSArray *)colors gradientStartPoint:(CGPoint)startPoint endPoint:(CGPoint)endPoint;
+
+
+
+
 @end
--- 奖励渐变/ViewController.m
+++ 奖励渐变/ViewController.m
@@ -11,6 +11,9 @@
 
 @interface ViewController ()<CAAnimationDelegate>
 
+@property (nonatomic,strong)UILabel *label;
+
+@property (nonatomic,strong)UIButton *btn;
 
 
 @end
@@ -19,9 +22,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor yellowColor];
+    self.view.backgroundColor = [UIColor whiteColor];
     
-
+    /*方法1*/
     
     UILabel* testLabel = [[UILabel alloc] initWithFrame:CGRectMake(100, 200, 400, 50)];
     testLabel.text = @"label上渐变方法1";
@@ -30,15 +33,24 @@
     [Healp TextGradientview:testLabel bgVIew:self.view gradientColors:@[(id)[UIColor redColor].CGColor, (id)[UIColor greenColor].CGColor, (id)[UIColor blueColor].CGColor] gradientStartPoint:CGPointMake(0, 1) endPoint:CGPointMake(1, 1)];
     
     
+    /*方法2*/
+    _label=[[UILabel alloc]initWithFrame:CGRectMake(10, 300, self.view.frame.size.width-20,50)];
+    [self.view addSubview:_label];
+    _label.text=@"label上渐变方法2";
+    _label.textAlignment=NSTextAlignmentCenter;
+    _label.font = [UIFont systemFontOfSize:30];
+    _label.textColor=[UIColor colorWithPatternImage:[UIImage imageNamed:@"gradient"]];
     
     
     
+    _btn=[[UIButton alloc]initWithFrame:CGRectMake(10, 350, self.view.frame.size.width-20, 100)];
+    [self.view addSubview:_btn];
+    _btn.titleLabel.font=[UIFont systemFontOfSize:30];
+    _btn.titleLabel.numberOfLines=0;
+    [_btn setTitle:@"button上字体渐变色设置" forState:UIControlStateNormal];
+    [Healp TextGradientControl:_btn bgVIew:self.view gradientColors:@[(id)[UIColor redColor].CGColor, (id)[UIColor greenColor].CGColor, (id)[UIColor blueColor].CGColor] gradientStartPoint:CGPointMake(0, 1) endPoint:CGPointMake(1, 1)];
     
     
-    
-    
-    
- 
 }
 
 
